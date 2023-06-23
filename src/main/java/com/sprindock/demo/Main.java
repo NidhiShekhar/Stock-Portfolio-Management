54ea9bb57689db46d2435c5b85472dd83e6aed95
Avi Mehta <avi.mehta1229@gmail.com>
2023-06-23 13:33:57 +0530

    Added PostgreSQL JDBC with Spring Data JPA

--- src/main/java/com/sprindock/demo/Main.java
+++ src/main/java/com/sprindock/demo/Main.java
@@ -2,11 +2,17 @@ package com.sprindock.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.jdbc.DataSourceBuilder;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// import javax.sql.DataSource;
+
 @SpringBootApplication
 @RestController
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -17,5 +23,18 @@ public class Main {
 	public String greet(){
 		return "Hello";
 	}
+
+//	@Configuration
+//	public static class DataSourceConfig {
+//		@Bean
+//		public DataSource getDataSource() {
+//			return DataSourceBuilder.create()
+//					.driverClassName("org.postgresql.Driver")
+//					.url("jdbc:postgresql://localhost:5432/customer_test")
+//					.username("avimehta")
+//					.password("12345678")
+//					.build();
+//		}
+//	}
 
 }
