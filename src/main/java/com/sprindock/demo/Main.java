4bf43e41b6eb0d1009af736c6c0887d0dec91d5e
Avi Mehta <avi.mehta1229@gmail.com>
2023-06-23 17:59:09 +0530

    Records added in database and created DOA Layer

--- src/main/java/com/sprindock/demo/Main.java
+++ src/main/java/com/sprindock/demo/Main.java
@@ -1,27 +1,63 @@
 package com.sprindock.demo;
 
+// import org.hibernate.mapping.List;
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 // import org.springframework.boot.jdbc.DataSourceBuilder;
 // import org.springframework.context.annotation.Bean;
 // import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // import javax.sql.DataSource;
 
 @SpringBootApplication
 @RestController
-
+@RequestMapping("/api/v1/customers")
 public class Main {
+
+	private final CustomerRepo customerRepository;
+
+	public Main(CustomerRepo customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 
-	@GetMapping("/")
-	public String greet(){
-		return "Hello";
+
+	@GetMapping
+	public List<Customer> getCustomers(){
+		return customerRepository.findAll();
+	}
+
+	record AddCustomerRequest(
+		String name,
+		String email,
+		Integer age
+	) {
+	}
+
+	@PostMapping
+	public void addCustomer(@RequestBody AddCustomerRequest request) {
+		Customer customer = new Customer();
+		customer.setName(request.name());
+		customer.setEmail(request.email());
+		customer.setAge(request.age());
+		customerRepository.save(customer);		
+	}
+
+	@DeleteMapping("{customerId}")
+	public void deleteCustomer(@PathVariable("customerId") Integer id){
+		customerRepository.deleteById(id);
 	}
 
 //	@Configuration
